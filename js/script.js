82aaa1f79dc9455d0fc318c0f922c9d7faeb05ed
Corfaya <lauracorrado0696@gmail.com>
2024-06-24 14:17:33 +0200

    [UPD] innerHTML property - to assign the last variable to #traits

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ let firstName = prompt("Inserisci il tuo nome");
 let lastName = prompt("Inserisci il tuo cognome");
 let favoriteColor = prompt("Inserisci il tuo colore preferito");
 let allAnswers = `${firstName}${lastName}${favoriteColor}23`
+
+// allAnswers assignment to #traits
+document.getElementById("traits").innerHTML = allAnswers;
