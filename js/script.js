8843167e496c0177d1faecb09a7d89cba0e88416
Corfaya <lauracorrado0696@gmail.com>
2024-06-24 14:15:18 +0200

    [UPD] variables declaration and initialization

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Variables
+let firstName = prompt("Inserisci il tuo nome");
+let lastName = prompt("Inserisci il tuo cognome");
+let favoriteColor = prompt("Inserisci il tuo colore preferito");
+let allAnswers = `${firstName}${lastName}${favoriteColor}23`
