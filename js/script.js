2f94cb0ee70ede0f3b3805bf9da2475b579659fb
Corfaya <lauracorrado0696@gmail.com>
2024-06-24 14:22:11 +0200

    [UPD] Review on console through console.log()

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // Variables
 let firstName = prompt("Inserisci il tuo nome");
+console.log(firstName)
 let lastName = prompt("Inserisci il tuo cognome");
+console.log(lastName)
 let favoriteColor = prompt("Inserisci il tuo colore preferito");
-let allAnswers = `${firstName}${lastName}${favoriteColor}23`
+console.log(favoriteColor)
+let allAnswers = `${firstName}${lastName}${favoriteColor}23`;
+console.log(allAnswers)
 
 // allAnswers assignment to #traits
 document.getElementById("traits").innerHTML = allAnswers;
